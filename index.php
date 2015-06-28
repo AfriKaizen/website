f7da662682c60082eac39273fc84c3cd6758fbd2
Steven Dumbura <stevedumbura@gmail.com>
2015-06-28 19:12:47 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+<title>AfriKaizen Inc. : eCommerce | Web | Mobile | Cloud & Custom Business Solutions</title>
+<link rel="stylesheet" type="text/css" href="uiux/semantic-ui/semantic.min.css">
+<link rel="stylesheet" type="text/css" href="uiux/css/style.min.css">
+<link href='http://fonts.googleapis.com/css?family=Source+Serif+Pro' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<body id="site" class="index pushable" ontouchstart>
+  <div class="pusher">
+    <div class="full height">
+      <div id="pc-menu" class="following bar light fixed">
+      <div class="ui page grid">
+        <div id="top-column" class="column">
+<!-- logo area -->
+          <div id="logo" class="ui logo">
+            <a class="ui item" href="http://www.afrikaizen.co.zw/">AfriKaizen Inc.</a>
+          </div>
+<!-- right floated content -->
+          <div id="fixed-menu" class="ui right floated secondary menu inverted">
+            <a class="ui item" href=""></a>
+            <a class="ui item" href=""></a>
+            <a class="ui item" href=""></a>
+          </div>
+          <div class="ui right floated secondary menu inverted">
+          </div>
+
+<!-- left floated content -->
+          <div class="ui large secondary network menu inverted">
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div id="content" class="content">
+      <div id="main-content" class="ui stackable responsive grid">
+        <div class="row" align="center">
+          <div  id="intro">
+          <div class="ui two column centered grid">
+            <div class="column">
+              <p align="center">Hello World. <br/> We are AfriKaizen Inc.</p>
+                <p align="left">
+                We believe in sustainable development through building handcrafted 
+                solutions to problems within the African environment.
+                Through the Kaizen Approach of “continuous improvement”, we work with businesses
+                no matter what size, to develop customised solutions that help them them grow and
+                leverage the power of the internet, the cloud, big data, mobile & e-commerce to propell
+                them into the future.
+              </p>
+              <p align="center">
+                "...inventing tommorrow..."
+                </p>
+            </div>
+            <div class="column">
+              <img style="float:right;" class="ui medium circular image" src="uiux/img/afrikaizen.png">
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <div class="row">
+      <div id="main-content" class="ui stackable responsive grid">
+      <br/>
+        <div class="row" align="center">
+          <p align="center">
+            <h1>We take technological solutions.</h1>
+            <h3>to the next level.</h3>
+          </p>
+        <br/>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="ui three column grid">
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="ui slide masked reveal image">
+                <img src="uiux/img/sans.png" class="visible content">
+                <img src="uiux/img/sans_hidden.png" class="hidden content">
+              </div>
+              <div class="content">
+                <a class="header">SANS Exposure</a>
+                <div class="meta">
+                  <a class="date" href="https://www.sans.co.zw/">Visit Site</a>
+                </div>
+                <div class="description">
+                  An e-commerce platform for Zimbabwean designers to sell their 
+                  products to local & oversease markets.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="ui slide masked reveal image">
+                <img src="uiux/img/zifund.png" class="visible content">
+                <img src="uiux/img/zifund_hidden.png" class="hidden content">
+              </div>
+              <div class="content">
+                <a class="header">ZiFund</a>
+                <div class="meta">
+                  <a class="date" href="http://www.zifund.co.zw/">Visit Site</a>
+                </div>
+                <div class="description">
+                  An online crowdfunding platform for Zimbabweans to invest in
+                  innovative Zimbabwean businesses & fund projects.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="ui slide masked reveal image">
+                <img src="uiux/img/ucoz.jpg" class="visible content">
+                <img src="uiux/img/ucoz_hidden.png" class="hidden content">
+              </div>
+              <div class="content">
+                <a class="header">UCOZ</a>
+                <div class="meta">
+                  <a class="date" href="http://www.ucoz.co.zw/">Visit Site</a>
+                </div>
+                <div class="description">
+                  A web based project for the United Commuter Operators Zimbabwe (UCOZ).
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+
+        <div class="row">
+          <div id="main-content" class="ui stackable responsive grid">
+          <br/>
+            <div class="row" align="center">
+              <p align="center">
+                <h1>Services.</h1>
+              </p>
+            <br/>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+        <div class="ui three column grid">
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="image">
+                <img src="uiux/img/1.png">
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="image">
+                <img src="uiux/img/2.png">
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="image">
+                <img src="uiux/img/3.png">
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+            <div class="row">
+          <div id="main-content" class="ui stackable responsive grid">
+          <br/>
+            <div class="row" align="center">
+              <p align="center">
+                <h1>Contact Us.</h1>
+                <br/>
+                <h2>Address:<br/>
+                16 Kew Drive, Highlands, Harare</h2>
+
+                <h2>Email:<br/>
+                info@afrikaizen.co.zw</h2>
+              </p><br/>
+
+                <tt>Copyright © AfriKaizen Inc. 2015</tt>
+              
+            <br/>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+<script type="text/javascript" src="uiux/semantic-ui/semantic.min.js"></script>
+<script type="text/javascript" src="uiux/js/main.min.js"></script>
+</body>
+
+
+
+
