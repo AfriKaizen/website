a8c0c246f105b5050ef6fb152f4d85bb119a7ac6
thetechrebel <stevedumbura@gmail.com>
2025-09-26 07:57:45 +0200

    website updates

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,19 @@ export default function Page() {
       <p className="mb-4">
           <span className='text-xl'>Hi 👋🏾<br/>
           We are AfriKaizen.<br/>
-          We are a tech-foused digital agency building cool things out of Southern Africa. 🌍<br/>
+          A tech-foused digital agency building cool things out of Southern Africa. 🌍<br/>
           </span>
 
-          <br/><br/>
+          <br/>
           We love building solutions for tough challenges. <span className='italic'>We also believe in keeping things simple.</span><br/><br/>
-          We believe <span className='font-mono'>digital transformation</span> does not have to give you headaches. <br/>
+
           We have worked with businesses, agencies, startups & individuals, turning their dreams and ideas into working apps and solutions that have impacted them forever.
-          We deliver on time, on budget and always exceed expectations. <br/><br/><br/>
+          We deliver on time, on budget and always exceed expectations. <br/><br/>
           
           Reach out to us today, we would love to help you build your next big thing!<br/>
-          Drop us an email: <a className='underline' href="mailto:hi@afrikaizen.co.zw">hi@afrikaizen.co.zw</a> and we can schedule a call.
+          Drop us an email: <a className='underline' href="mailto:hi@afrikaizen.co.zw">hi@afrikaizen.co.zw</a> and we can schedule a call.<br/><br/>
 
-          <br/><br/><br/>
-          If you would like to see what we do, here is some of the work we've done 👇🏾
+          Here is some of the cool stuff we've done for our clients & partners 👇🏾
       </p>
       <div className="my-8">
         <BlogPosts />
